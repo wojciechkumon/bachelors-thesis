cc145357eacc93688ef51b0f30f768b157dec963
Wojciech Kumoń <wojtas626@gmail.com>
2017-07-06 23:41:32 +0200

    Jni enhancement

--- transfer_tester/src/main/c/JniExecutor.c
+++ transfer_tester/src/main/c/JniExecutor.c
@@ -1,8 +1,8 @@
 #include <jni.h>
 #include <stdio.h>
-#include "pl_kumon_transfertester_tester_JniExcecutor.h"
+#include "pl_kumon_transfertester_tester_jni_JniExcecutor.h"
 
-JNIEXPORT jstring JNICALL Java_pl_kumon_transfertester_tester_JniExcecutor_stringFromJni(JNIEnv *env, jobject thisObj) {
+JNIEXPORT jstring JNICALL Java_pl_kumon_transfertester_tester_jni_JniExcecutor_stringFromJni(JNIEnv *env, jobject thisObj) {
     printf("Hello JNI World!\n");
     return NULL;
 }
--- transfer_tester/src/main/c/c_compile.sh
+++ transfer_tester/src/main/c/c_compile.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# javah -jni pl.kumon.transfertester.tester.jni.JniExcecutor
+
 cc -c -I/System/Library/Frameworks/JavaVM.framework/Headers JniExecutor.c
 cc -dynamiclib -o ././../../../target/classes/JniExecutor.jnilib JniExecutor.o -framework JavaVM
 
