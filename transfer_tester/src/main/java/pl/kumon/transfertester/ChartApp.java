0e2c27b330b84f4ef0ec87ab20fd5ad6eaaf9748
Wojciech Kumoń <wojtas626@gmail.com>
2017-09-10 23:36:32 +0200

    Metrics grouped by sent data size and test type.

--- transfer_tester/src/main/java/pl/kumon/transfertester/ChartApp.java
+++ transfer_tester/src/main/java/pl/kumon/transfertester/ChartApp.java
@@ -1,12 +1,16 @@
 package pl.kumon.transfertester;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
 import pl.kumon.transfertester.csv.CsvReader;
 import pl.kumon.transfertester.metrics.Metrics;
+import pl.kumon.transfertester.tester.TestType;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import io.reactivex.Observable;
+import io.reactivex.observables.GroupedObservable;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -16,8 +20,28 @@ public class ChartApp implements Runnable {
   @Override
   public void run() {
     Path csvDir = Paths.get(appProps.getOrDefault("csvSourceDirectory", "csv"));
-    Observable<Metrics> metrics = new CsvReader()
-        .parseMetricInDir(csvDir);
-    System.out.println("Number of metrics: " + metrics.count().blockingGet());
+    new CsvReader()
+        .parseMetricInDir(csvDir)
+        .groupBy(metrics -> ImmutablePair.of(metrics.getRequestSize(), metrics.getResponseSize()))
+        .subscribe(
+            metricsGroup -> handleMetricsGroupedByDataSize(metricsGroup,
+                metricsGroup.getKey().getLeft(),
+                metricsGroup.getKey().getRight()));
+
+  }
+
+  private void handleMetricsGroupedByDataSize(GroupedObservable<? extends Pair<Integer, Integer>, Metrics> metricsGroup,
+                                              int requestSize, int responseSize) {
+    metricsGroup
+        .groupBy(Metrics::getTestType)
+        .subscribe(metrics -> handleMetricsGroupedByDataSizeAnTestType(metrics, requestSize,
+            responseSize, metrics.getKey()));
+  }
+
+  private void handleMetricsGroupedByDataSizeAnTestType(GroupedObservable<TestType, Metrics> groupedMetrics,
+                                                        int requestSize, int responseSize, TestType testType) {
+    groupedMetrics.reduce(0, (integer, metrics) -> integer + 1)
+        .subscribe(amount -> System.out.println(testType + " "
+            + requestSize + " " + responseSize + " " + amount));
   }
 }
