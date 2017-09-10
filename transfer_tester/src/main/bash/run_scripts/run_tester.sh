bb852d1bffaaf028697a717beb159554242b9e0f
Wojciech Kumoń <wojtas626@gmail.com>
2017-09-10 20:26:09 +0200

    Bash run script generating many csv files

--- transfer_tester/src/main/bash/run_scripts/run_tester.sh
+++ transfer_tester/src/main/bash/run_scripts/run_tester.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-java -jar transfer-tester.jar testType=JNI requestSize=8192 responseSize=8192 numberOfTests=1000 warmUpTests=100 csvFilePath=csv/jni_8192_8192.csv
+function runTester { # testType, requestSize, responseSize, numberOfTests, warmUpTests, csvDir
+    java -jar transfer-tester.jar testType=${1} requestSize=${2} responseSize=${3} numberOfTests=${4} warmUpTests=${5} csvFilePath=${6}/${1}_${2}_${3}.csv
+}
+
+function runTesterWithDefaultRequestResponseSizes { # testType, numberOfTests, warmUpTests, csvDir
+    runTester ${1} 16 16 ${2} ${3} ${4} # 16B
+    runTester ${1} 1024 1024 ${2} ${3} ${4} # 1KB
+    runTester ${1} 8192 8192 ${2} ${3} ${4} # 8KB
+    runTester ${1} 262144 262144 ${2} ${3} ${4} # 256KB
+    runTester ${1} 1048576 1048576 ${2} ${3} ${4} # 1MB
+}
+
+runTesterWithDefaultRequestResponseSizes JNI 1000 100 csv
